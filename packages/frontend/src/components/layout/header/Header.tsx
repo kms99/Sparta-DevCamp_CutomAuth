a3aabc6cb07a9494d282c7c5c0fc50b27d8f27df
Kim MyungSub <audtjqxx@naver.com>
2024-04-27 02:09:34 +0900

    [refactor]: backend, frontend 모노레포구조로 변경

--- packages/frontend/src/components/layout/header/Header.tsx
+++ packages/frontend/src/components/layout/header/Header.tsx
@@ -6,7 +6,7 @@ import HeaderItem from "./HeaderItem";
 const Header = () => {
   //TODO: 로그인 세션에 따른 버튼 조건부 처리
   return (
-    <header className="flex items-center justify-between px-5 py-2">
+    <header className="fixed top-0 left-0 w-full flex items-center justify-between px-5 py-2">
       <h1 className="text-xl font-bold" about="title">
         IN&UP
       </h1>
