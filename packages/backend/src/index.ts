9b9c63a95821b606778fdccb23a7c1cbe153915c
Kim MyungSub <audtjqxx@naver.com>
2024-04-28 01:08:41 +0900

    [backend/feat]: 로그인, 회원가입 로직 완성, frontend와 연동

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -21,8 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("public", serveStatic(path.join(__dirname, "public")));
 
-app.post("/process/user", (req, res) => {
-  console.log("/process/user 호출됨");
+/**
+ * 회원가입
+ */
+app.post("/process/user/sign-up", (req, res) => {
+  console.log("/process/user/sign-up 호출됨");
 
   const { nickname, email, role, phone, password } = req.body;
 
@@ -31,36 +34,104 @@ app.post("/process/user", (req, res) => {
       if (conn) {
         conn.release();
       }
-      console.log("MySQL getConnection error: ", err);
+      console.log(`${Date.now()} / [서버 오류] DB와 연결할 수 없습니다.`);
+      res
+        .status(500)
+        .json({ message: "서버 오류가 발생하였습니다. 관리자에 문의하세요" });
       return;
     }
 
     console.log("connect database");
 
-    const exec = conn.query(
+    conn.query(
       "INSERT INTO users(nickname,email,role,phone,password) VALUES (?,?,?,?,SHA2(?,256));",
       [nickname, email, role, phone, password],
       (err, result) => {
         conn.release();
-        console.log("실행된 SQL: " + exec.sql);
 
         if (err) {
-          console.log("SQL 실행 오류");
-          console.dir(err);
+          console.log(
+            `${Date.now()} / [DB 오류] DATABASE query가 잘못되었습니다.`
+          );
+          res
+            .status(500)
+            .json({ message: "DB 오류가 발생하였습니다. 관리자에 문의하세요" });
+          return;
         }
+
         if (result) {
-          console.dir(result);
-          console.log("SQL 실행 성공");
-
-          res.writeHead("200", { "Content-Type": "text/html; charset=utf8" });
-          res.write("<h2>사용자 추가 성공</h2>");
-          res.send;
+          console.log(`${Date.now()} / [회원가입 성공] ${nickname}(${email})`);
+          res.status(200).json({
+            message: "회원가입 성공",
+            user: { nickname, email, role, phone },
+          });
+          return;
         } else {
-          console.log("SQL 실행 실패");
+          console.log(
+            `${Date.now()} / [회원가입 실패] (input: ${email}) 이미 생성된 계정이 있습니다.`
+          );
+          res
+            .status(404)
+            .json({ message: "회원가입 실패, 이미 생성된 계정이 있습니다." });
+          return;
+        }
+      }
+    );
+  });
+});
 
-          res.writeHead("200", { "Content-Type": "text/html; charset=utf8" });
-          res.write("<h2>사용자 추가 실패</h2>");
-          res.send;
+/**
+ * 로그인
+ */
+app.post("/process/user/sign-in", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log("/process/user/sign-in 호출됨, 로그인 요청");
+
+  pool.getConnection((err, conn) => {
+    if (err) {
+      if (conn) {
+        conn.release();
+      }
+      console.log(`${Date.now()} / [서버 오류] DB와 연결할 수 없습니다.`);
+      res
+        .status(500)
+        .json({ message: "서버 오류가 발생하였습니다. 관리자에 문의하세요" });
+      return;
+    }
+
+    console.log("connect database");
+
+    conn.query(
+      "SELECT * FROM users WHERE email=? AND password=SHA2(?,256);",
+      [email, password],
+      (err, rows) => {
+        // connect 해제
+        conn.release();
+        if (err) {
+          console.log(
+            `${Date.now()} / [DB 오류] DATABASE query가 잘못되었습니다.`
+          );
+          res
+            .status(500)
+            .json({ message: "DB 오류가 발생하였습니다. 관리자에 문의하세요" });
+          return;
+        }
+
+        if (rows.length > 0) {
+          console.log(
+            `${Date.now()} / [로그인 성공] ${rows[0].nickname}(${rows[0].email})`
+          );
+          res.status(200).json({ message: "로그인 성공", user: rows[0] });
+          return;
+        } else {
+          console.log(
+            `${Date.now()} / [로그인 실패] (input: ${email}) 입력한 정보와 일치하는 정보가 없습니다.`
+          );
+          res
+            .status(404)
+            .json({ message: "로그인 실패, 일치하는 사용자가 없습니다." });
+          return;
         }
       }
     );
